c38082942e87893063d3041edec682dd8fd0898a
Tanawat Tassana <amp.tanawat@gmail.com>
2016-12-09 01:13:29 +0700

    Add Address and Entry

--- src/Data/AddressBook.purs
+++ src/Data/AddressBook.purs
@@ -5,3 +5,24 @@ import Prelude
 import Control.Plus (empty)
 import Data.List (List(..), filter, head)
 import Data.Maybe (Maybe)
+
+type Address = {
+    street :: String,
+    country :: String
+}
+
+type Entry = {
+    firstName :: String,
+    lastName :: String,
+    address :: Address
+}
+
+type AddressBook = List Entry
+
+showAddress :: Address -> String
+showAddress addr = addr.street <> ", " <> addr.country
+
+showEntry :: Entry -> String
+showEntry entry = entry.firstName <> " " <>
+    entry.lastName <> ", " <>
+    showAddress entry.address
